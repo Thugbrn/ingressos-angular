d822afdbdd88eeb62b7e9cfdc0aac9c8b70b88b1
Thugbrn <thugbrn@gmail.com>
2023-10-08 13:38:51 -0300

    componente header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
+  standalone: true, 
+  imports: [RouterLink, RouterOutlet, CommonModule],
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from 'src/app/components/header/header.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
+  standalone: true,
+  imports: [HeaderComponent, CommonModule],
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
