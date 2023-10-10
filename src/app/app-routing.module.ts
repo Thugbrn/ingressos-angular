3a094f0576086ff311580b29e7569fc1c55040da
afsantos2 <anderson_f.ads@hotmail.com>
2023-10-09 22:19:42 -0300

    adiciona rotas, home e contato

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ContatoComponent } from './pages/contato/contato.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'contato',
+    component: ContatoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
